759dd70268fbfda5bf2f225ece71d276cefd441b
mayue13 <mayurdahak13@gmail.com>
2022-05-28 09:55:22 +0530

    CSS Added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
-function ExpenseItem(){
-    return (
-        <h2>Expense Item</h2>
-    );
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date().toLocaleDateString();
+  const expenseTitle="Car Insurance";
+  const expenseAmount=2323.2;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>      
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
